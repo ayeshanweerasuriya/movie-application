dc575c90d8c6b1ec72e1a46c6f73a6676233317c
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-11-26 21:04:39 +0530

    Linked API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
+
+// 9e358063
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9e358063";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("John Wick");
+  }, []);
   return (
     <>
-      <h1>App</h1>
-      <h1>App</h1>
-      <h1>App</h1>
       <h1>App</h1>
     </>
   );
